07f3f9b9ae197472ca2b01a256e80e66d7bc3ac5
fariraimasocha <114150009+fariraimasocha@users.noreply.github.com>
2023-05-04 15:51:49 +0200

    update portfolio
    my portfolio

---
+++ script.js
@@ -0,0 +1,5 @@
+const textContainer = document.querySelector('.text-container');
+
+textContainer.classList.remove('-translate-x-full', 'opacity-0');
+textContainer.classList.add('opacity-100', 'delay-500', 'ease-out');
+
